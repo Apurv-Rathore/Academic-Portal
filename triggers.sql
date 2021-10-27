93157b89cbb09b2075d0719df5f4756d887ba2f7
Apurv Rathore <thelegend2710@gmail.com>
2021-10-27 09:26:12 +0530

    done

--- triggers.sql
+++ triggers.sql
@@ -18,3 +18,29 @@ CREATE TRIGGER check_student_insert
     AFTER INSERT ON student
     FOR EACH ROW
     EXECUTE PROCEDURE create_student_tables();
+
+
+
+
+CREATE or REPLACE FUNCTION create_section_grade_table() 
+    RETURNS TRIGGER 
+    LANGUAGE PLPGSQL
+AS $$
+DECLARE
+    s1 varchar;
+    s2 varchar;
+    cond integer;
+BEGIN
+    -- s2 = CONCAT('section_grades_', NEW.offering_id);
+    s1 = CONCAT('CREATE TABLE IF NOT EXISTS section_grades_', NEW.offering_id ,' ( student_id varchar(10) not null, grade integer)');
+    
+    execute s1;
+RETURN NULL;
+END;
+$$;
+
+
+CREATE TRIGGER check_section_grade_table
+    AFTER INSERT ON course_offering
+    FOR EACH ROW
+    EXECUTE PROCEDURE create_section_grade_table();
