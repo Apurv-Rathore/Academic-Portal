ca03acd997409beb56d0c8f47bd85a855f5e8abb
Apurv Rathore <thelegend2710@gmail.com>
2021-10-27 16:58:41 +0530

    fsdf

--- code.sql
+++ code.sql
@@ -1,5 +1,5 @@
 CREATE TABLE course_catalogue (
-    course_id varchar(10) not null,
+    course_id varchar(20) not null,
     name varchar(50) not null,
     L integer not null,
     T integer not null,
@@ -10,16 +10,16 @@ CREATE TABLE course_catalogue (
 );
 
 CREATE TABLE prerequisites (
-    course_id varchar(10) not null,
-    prerequisite_course_id varchar(10) not null,
+    course_id varchar(20) not null,
+    prerequisite_course_id varchar(20) not null,
     FOREIGN KEY (course_id) REFERENCES course_catalogue(course_id),
     FOREIGN KEY (prerequisite_course_id) REFERENCES course_catalogue(course_id)
 );
 
-CREATE TABLE department(name varchar(10), PRIMARY KEY (name));
+CREATE TABLE department(name varchar(20), PRIMARY KEY (name));
 
 CREATE TABLE instructor (
-    instructor_id varchar(10) not null,
+    instructor_id varchar(20) not null,
     name varchar(50) not null,
     dept_name varchar(20),
     PRIMARY KEY (instructor_id),
@@ -40,26 +40,25 @@ CREATE TABLE time_slots(
     friday_end TIME 
 );
 
-Create table course_offering(
-    offering_id serial PRIMARY KEY,
-    course_id varchar(10) not null,
-    year integer not null,
-    semester integer not null,
-    section_id varchar(10) not null,
-    instructor_id varchar(10) not null,
-    slot_number integer not null,
-    classroom varchar(10) not null,
-    cgpa_requirement double precision,
-    FOREIGN KEY (instructor_id) REFERENCES instructor(instructor_id),
-    FOREIGN KEY (course_id) REFERENCES course_catalogue(course_id),
-    FOREIGN KEY (slot_number) REFERENCES time_slots(slot_number)
-    -- PRIMARY KEY at top
-);
+-- Create table course_offering(
+--     offering_id serial PRIMARY KEY,
+--     course_id varchar(20) not null,
+--     year integer not null,
+--     semester integer not null,
+--     section_id varchar(20) not null,
+--     instructor_id varchar(20) not null,
+--     slot_number integer not null,
+--     cgpa_requirement double precision,
+--     FOREIGN KEY (instructor_id) REFERENCES instructor(instructor_id),
+--     FOREIGN KEY (course_id) REFERENCES course_catalogue(course_id),
+--     FOREIGN KEY (slot_number) REFERENCES time_slots(slot_number)
+--     -- PRIMARY KEY at top
+-- );
 
 CREATE TABLE student(
-    student_id varchar(10) not null,
+    student_id varchar(20) not null,
     name varchar(50) not null,
-    dept_name varchar(10) not null,
+    dept_name varchar(20) not null,
     CGPA double precision,
     credit1 double precision,
     credit2 double precision,
@@ -67,76 +66,77 @@ CREATE TABLE student(
     PRIMARY KEY (student_id)
 );
 
-CREATE TABLE taken(
-    student_id varchar(10) not null,
-    offering_id integer not null,
-    FOREIGN KEY (offering_id) REFERENCES course_offering (offering_id),
-    FOREIGN KEY (student_id) REFERENCES student(student_id),
-    PRIMARY KEY (student_id, offering_id)
+-- dynamicly created
+CREATE TABLE course_offering_instid(
+    course_id varchar(20) not null,
+    year integer not null,
+    semester integer not null,
+    section_id varchar(20) not null,
+    slot_number integer not null,
+    cgpa_requirement double precision,
+    PRIMARY KEY (year, semester, section_id, course_id),
+    FOREIGN KEY (course_id) REFERENCES course_catalogue(course_id),
+    FOREIGN KEY (slot_number) REFERENCES time_slots(slot_number)
 );
 
 CREATE TABLE student_transcript(
     -- student_transcript_student_id
-    offering_id integer,
+    course_id integer,
     grade integer,
-    FOREIGN KEY (offering_id) REFERENCES course_offering (offering_id),
-    PRIMARY KEY (offering_id)
+    PRIMARY KEY (course_id)
 );
 
 CREATE TABLE section_offered_grades(
     -- section_offered_grades_offering id
-    student_id varchar(10) not null,
+    student_id varchar(20) not null,
     grade integer,
     FOREIGN KEY (student_id) REFERENCES student(student_id),
     PRIMARY KEY (student_id)
 );
 
 CREATE TABLE instructor_ticket_table(
-    offering_id integer not null,
-    student_id varchar(10) not null,
-    PRIMARY KEY (offering_id, student_id),
-    FOREIGN KEY (student_id) REFERENCES student(student_id),
-    FOREIGN KEY (offering_id) REFERENCES course_offering(offering_id)
+    student_id varchar(20) not null,
+    PRIMARY KEY (student_id),
+    FOREIGN KEY (student_id) REFERENCES student(student_id)
 );
 
 CREATE TABLE dean_academics_ticket_table(
-    offering_id integer not null,
-    student_id varchar(10) not null,
+    year integer not null,
+    semester integer not null,
+    section_id varchar(20) not null,
+    course_id varchar(20) not null, 
+    student_id varchar(20) not null,
     has_accepted_instructor boolean,
     has_accepted_batch_advisor boolean,
-    PRIMARY KEY (offering_id, student_id),
-    FOREIGN KEY (student_id) REFERENCES student(student_id),
-    FOREIGN KEY (offering_id) REFERENCES course_offering(offering_id)
+    PRIMARY KEY (year, semester, section_id, course_id, student_id),
+    FOREIGN KEY (student_id) REFERENCES student(student_id)
 );
 
 CREATE TABLE batch_advisor_ticket_table(
-    student_id varchar(10) not null,
-    offering_id integer not null,
+    student_id varchar(20) not null,
+    year integer not null,
+    semester integer not null,
+    section_id varchar(20) not null,
+    course_id varchar(20) not null, 
     has_accepted_instructor boolean,
-    PRIMARY KEY (offering_id, student_id),
-    FOREIGN KEY (student_id) REFERENCES student(student_id),
-    FOREIGN KEY (offering_id) REFERENCES course_offering(offering_id)
+    PRIMARY KEY (year, semester, section_id, course_id, student_id),
+    FOREIGN KEY (student_id) REFERENCES student(student_id)
 );
 
 CREATE TABLE student_ticket_table(
-    student_id varchar(10) not null,
-    offering_id integer not null,
+    student_id varchar(20) not null,
+    year integer not null,
+    semester integer not null,
+    section_id varchar(20) not null,
+    course_id varchar(20) not null, 
     has_accepted boolean,
-    PRIMARY KEY (offering_id, student_id),
-    FOREIGN KEY (student_id) REFERENCES student(student_id),
-    FOREIGN KEY (offering_id) REFERENCES course_offering(offering_id)
+    PRIMARY KEY (year, semester, section_id, course_id, student_id),
+    FOREIGN KEY (student_id) REFERENCES student(student_id)
 );
 
 CREATE TABLE course_completed(
-    student_id varchar(10) not null,
-    course_id varchar(10) not null
-);
-
-CREATE TABLE allowed_batches_for_offering(
-    offering_id integer not null,
-	batch integer not null,
-	PRIMARY KEY(offering_id, batch)	,
-	FOREIGN KEY (offering_id) REFERENCES course_offering(offering_id)
+    student_id varchar(20) not null,
+    course_id varchar(20) not null
 );
 
 
--- roles.sql
+++ roles.sql
@@ -17,7 +17,7 @@ create ROLE faculty WITH
 	NOREPLICATION 
 	CONNECTION LIMIT -1;
     
-create ROLE faculty WITH   
+create ROLE dean WITH   
 	LOGIN 
 	NOSUPERUSER 
 	NOCREATEDB 
@@ -26,11 +26,49 @@ create ROLE faculty WITH
 	NOREPLICATION 
 	CONNECTION LIMIT -1;
 
+
+
+-- SELECT , INSERT , UPDATE , DELETE , TRUNCATE , REFERENCES , TRIGGER , CREATE , CONNECT , TEMPORARY , EXECUTE , and USAGE 
+
+-- faculty
+GRANT CONNECT ON DATABASE project1 TO faculty;
 GRANT SELECT, REFERENCES ON course_catalogue to faculty;
-GRANT SELECT, INSERT, UPDATE, DELETE ON prerequisites to faculty;
+GRANT SELECT ON prerequisites to faculty;
+GRANT SELECT ON department to faculty;
 GRANT SELECT, REFERENCES ON department to faculty;
 GRANT SELECT ON instructor to faculty;
-GRANT SELECT, INSERT, UPDATE ON time_slots to faculty;
-GRANT SELECT, INSERT, UPDATE ON course_offering to faculty;
+GRANT SELECT, REFERENCES ON time_slots to faculty;
+-- GRANT SELECT, INSERT, UPDATE ON course_offering to faculty;
 GRANT SELECT ON instructor_ticket_table to faculty;
 
+
+-- student 
+GRANT CONNECT ON DATABASE project1 TO student_role;
+GRANT SELECT ON prerequisites to student_role;
+GRANT SELECT ON department to student_role;
+GRANT SELECT ON time_slots to student_role;
+GRANT USAGE ON SCHEMA project_schema TO student_role;
+GRANT SELECT ON TABLE instructor, course_offering, taken, student,  TO student_role;
+-- GRANT SELECT ON TABLE course_offering TO student_role;
+ALTER DEFAULT PRIVILEGES IN SCHEMA project_schema GRANT SELECT ON TABLES TO student_role;
+
+
+-- batch advisor
+GRANT CONNECT ON DATABASE project1 TO batch_advisor_role;
+GRANT USAGE, CREATE ON SCHEMA project_schema TO batch_advisor_role;
+GRANT SELECT ON TABLE course_catalogue, prerequisites, student, instructor, course_offering, taken, student_transcript, course_offered_grades, TO batch_advisor_role;
+-- SELECT ON TABLE course_offering to batch_advisor_role;
+GRANT SELECT ON time_slots to batch_advisor_role;
+GRANT SELECT ON department to batch_advisor_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE ____ TO batch_advisor_role;
+ALTER DEFAULT PRIVILEGES IN SCHEMA project_schema GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO batch_advisor_role;
+
+
+--  dean 
+
+GRANT SELECT, UPDATE, INSERT, DELETE, REFERENCES ON course_catalogue to dean_role;
+GRANT SELECT, UPDATE, INSERT, DELETE ON prerequisites to dean_role;
+GRANT SELECT, UPDATE, INSERT, DELETE ON department to dean_role;
+GRANT SELECT, UPDATE, INSERT, DELETE ON instructor to dean_role;
+GRANT SELECT, UPDATE, INSERT, DELETE ON time_slots to dean_role;
+-- GRANT SELECT on course_offering to dean_role;
