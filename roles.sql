219ff85a46dae5482f3ce29b7c1b91fac9ca11a1
Jaglike Makkar <58853798+jaglikemakkar@users.noreply.github.com>
2021-10-27 16:16:43 +0530

    Update roles.sql

--- roles.sql
+++ roles.sql
@@ -34,3 +34,13 @@ GRANT SELECT, INSERT, UPDATE ON time_slots to faculty;
 GRANT SELECT, INSERT, UPDATE ON course_offering to faculty;
 GRANT SELECT ON instructor_ticket_table to faculty;
 
+GRANT CONNECT ON DATABASE project1 TO student_role;
+GRANT USAGE ON SCHEMA project_schema TO student_role;
+GRANT SELECT ON TABLE instructor, course_offering, taken, student,  TO student_role;
+ALTER DEFAULT PRIVILEGES IN SCHEMA project_schema GRANT SELECT ON TABLES TO student_role;
+
+GRANT CONNECT ON DATABASE project1 TO batch_advisor_role;
+GRANT USAGE, CREATE ON SCHEMA project_schema TO batch_advisor_role;
+GRANT SELECT ON TABLE course_catalogue, prerequisites, student, instructor, course_offering, taken, student_transcript, course_offered_grades, TO batch_advisor_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE ____ TO batch_advisor_role;
+ALTER DEFAULT PRIVILEGES IN SCHEMA project_schema GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO batch_advisor_role;
