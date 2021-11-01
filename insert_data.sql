950c7a696a3f60a5556f2959a69d7469b4abd820
Apurv Rathore <2019csb1077@iitrpr.ac.in>
2021-11-01 22:31:49 +0530

    Update insert_data.sql

--- insert_data.sql
+++ insert_data.sql
@@ -37,24 +37,16 @@ values ('03:00:00', '03:50:00', '03:00:00', '03:50:00', '03:00:00', '03:50:00', 
 insert into time_slots(monday_start, monday_end, tuesday_start, tuesday_end, wednesday_start, wednesday_end, thursday_start, thursday_end, friday_start, friday_end)
 values ('04:00:00', '04:50:00', '04:00:00', '04:50:00', '04:00:00', '04:50:00', '04:00:00', '04:50:00', '04:00:00', '04:50:00');
 
-insert into course_offering(course_id, year, semester, section_id, instructor_id, slot_number, classroom, cgpa_requirement)
-values ('CS303', 2021, 1, 1, '2', 1, 'CS1', 6.0);
-insert into course_offering(course_id, year, semester, section_id, instructor_id, slot_number, classroom, cgpa_requirement)
-values ('CS302', 2021, 1, 1, '3', 1, 'CS2', 7.0);
-insert into course_offering(course_id, year, semester, section_id, instructor_id, slot_number, classroom)
-values ('CS301', 2021, 1, 1, '1', 1, 'CS1');
-insert into course_offering(course_id, year, semester, section_id, instructor_id, slot_number, classroom)
-values ('CS517', 2021, 1, 1, '4', 1, 'CS2');
+insert into course_offering(course_id, year, semester, section_id, instructor_id, slot_number, cgpa_requirement)
+values ('CS303', 2021, 1, 1, '2', 1, 6.0);
+insert into course_offering(course_id, year, semester, section_id, instructor_id, slot_number, cgpa_requirement)
+values ('CS302', 2021, 1, 1, '3', 1, 7.0);
+insert into course_offering(course_id, year, semester, section_id, instructor_id, slot_number)
+values ('CS301', 2021, 1, 1, '1', 1);
+insert into course_offering(course_id, year, semester, section_id, instructor_id, slot_number)
+values ('CS517', 2021, 1, 1, '4', 1);
 
 
 insert into student(student_id, name, dept_name,batch) values ('1', 'Jaglike Makkar', 'CSE',2019);
 insert into student(student_id, name, dept_name,batch) values ('2', 'Apurv Rathore', 'CSE',2019);
 insert into student(student_id, name, dept_name,batch) values ('3', 'Aman Chourasiya', 'CSE',2019);
-
-insert into taken(student_id, offering_id) values ('1', 2);
-insert into taken(student_id, offering_id) values ('1', 3);
-insert into taken(student_id, offering_id) values ('1', 4);
-insert into taken(student_id, offering_id) values ('2', 1);
-insert into taken(student_id, offering_id) values ('2', 3);
-insert into taken(student_id, offering_id) values ('3', 3);
-insert into taken(student_id, offering_id) values ('3', 4);
