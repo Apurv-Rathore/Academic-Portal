48c3ea7f8383cb06de4070f2b71ead4f116c71e0
Apurv Rathore <thelegend2710@gmail.com>
2021-11-04 13:01:25 +0530

    ;fws

--- stored_procedures.sql
+++ stored_procedures.sql
@@ -97,7 +97,7 @@ create or replace PROCEDURE offer_course(cgpa_constraint_inp double precision, c
 year_inp integer,
 semester_inp integer,
 section_id_inp varchar(10),
-allowed_batches INT[],
+allowed_batches_ INT[],
 time_slots_avaiable TIME[]) 
 AS
 $$
@@ -118,7 +118,6 @@ declare
     offering_id_record record;
 begin
 
-
     monday_start_var := time_slots_avaiable[0];
     monday_end_var := time_slots_avaiable[1];
     tuesday_start_var := time_slots_avaiable[2];
@@ -133,7 +132,6 @@ begin
     for i in 1..10 loop
         raise notice 'Value: %', time_slots_avaiable[i];
     end loop;
-
 
     select slot_number
     into variable_name 
@@ -153,9 +151,9 @@ begin
     
 
     INSERT INTO course_offering(
-        course_id, instructor_id, year, semester, section_id, slot_number, cgpa_requirement
+        course_id, instructor_id, year, semester, section_id, slot_number, cgpa_requirement, allowed_batches
     )
-    VALUES (course_idd, instructor_idd, year_inp, semester_inp, section_id_inp, variable_name.slot_number, cgpa_constraint_inp);
+    VALUES (course_idd, instructor_idd, year_inp, semester_inp, section_id_inp, variable_name.slot_number, cgpa_constraint_inp, allowed_batches_);
 
     select course_offering.offering_id 
     into offering_id_record 
@@ -171,10 +169,10 @@ begin
 
     offering_id := offering_id_record.offering_id;
 
-    array_length := array_length(allowed_batches,1);
-    for i in 1..array_length loop
-        insert into allowed_batches_for_offering(offering_id,batch) values (offering_id, allowed_batches[i]);
-    end loop;
+    -- array_length := array_length(allowed_batches,1);
+    -- for i in 1..array_length loop
+    --     insert into allowed_batches_for_offering(offering_id,batch) values (offering_id, allowed_batches[i]);
+    -- end loop;
 end;
 $$
 language plpgsql;
