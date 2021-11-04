9e71c2e179787f6e740a1522bc35cdd391fd44e6
Jaglike Makkar <58853798+jaglikemakkar@users.noreply.github.com>
2021-11-04 13:18:23 +0530

    Update roles.sql

--- roles.sql
+++ roles.sql
@@ -27,89 +27,101 @@ create ROLE dean_role WITH
 	CONNECTION LIMIT -1;
 
 
-
-
-
-
-
-
-
-
-
-
-
--- SELECT , INSERT , UPDATE , DELETE , TRUNCATE , REFERENCES , TRIGGER , CREATE , CONNECT , TEMPORARY , EXECUTE , and USAGE 
-
--- faculty_role
-GRANT CONNECT ON DATABASE project1 TO faculty_role;
-GRANT SELECT, REFERENCES ON course_catalogue to faculty_role;
-GRANT SELECT ON prerequisites to faculty_role;
-GRANT SELECT ON department to faculty_role;
-GRANT SELECT, REFERENCES ON department to faculty_role;
-GRANT SELECT ON instructor to faculty_role;
-GRANT SELECT, REFERENCES ON student to faculty_role;
-GRANT SELECT, REFERENCES ON time_slots to faculty_role;
-GRANT SELECT, UPDATE, INSERT, DELETE ON "course_offering_inst_id" to faculty_role;
-GRANT SELECT, UPDATE, INSERT, DELETE ON "student_transcript_student_id" to faculty_role;
-GRANT SELECT, UPDATE, INSERT, DELETE ON "course_year_sem_sec_grades" to faculty_role;
-GRANT SELECT, UPDATE, INSERT, DELETE ON "instructor_ticket_table_inst_id" to faculty_role;
-GRANT INSERT ON batch_advisor_ticket_table to faculty_role;
-
-
--- GRANT SELECT, INSERT, DELETE, UPDATE ON student_transcript to faculty; grant that instrcutor the premission to do these when a student resgisters on his course
-
--- GRANT SELECT, INSERT, UPDATE ON course_offering_instid to faculty;
-GRANT SELECT ON instructor_ticket_table to faculty;
-
-
 -- student 
 GRANT CONNECT ON DATABASE project1 TO student_role;
 GRANT SELECT ON course_catalogue to student_role;
 GRANT SELECT ON prerequisites to student_role;
 GRANT SELECT ON department to student_role;
-GRANT SELECT ON time_slots to student_role;
 GRANT SELECT ON instructor to student_role;
--- GRANT SELECT ON student to student_role;
-GRANT SELECT ON "course_offering_inst_id" to student_role;
-GRANT SELECT, INSERT ON "student_transcript_student_id" to student_role;
-GRANT INSERT ON "instructor_ticket_table_inst_id" to student_role;
+GRANT SELECT ON time_slots to student_role;
+GRANT SELECT ON student to student_role;
+GRANT SELECT ON course_offering to student_role;
+GRANT SELECT ON "course_offering_year_semester" to student_role;
+GRANT SELECT ON "student_transcript_student_id" to student_role;
+GRANT SELECT, INSERT ON "student_ticket_table_student_id" to student_role;
+GRANT SELECT ON "program_cores_batch_number" to student_role;
+GRANT SELECT ON "open_elective_batch_number" to student_role;
+GRANT SELECT ON "program_elective_batch_number" to student_role;
+GRANT SELECT ON "science_core_batch_number" to student_role;
+GRANT SELECT ON "credit_requirenment_to_graduate" to student_role;
 
 
--- GRANT USAGE ON SCHEMA project_schema TO student_role;
--- GRANT SELECT ON TABLE course_offering_instid TO student_role;
-
--- to do student_transcript
--- ALTER DEFAULT PRIVILEGES IN SCHEMA project_schema GRANT SELECT ON TABLES TO student_role;
-
-
---  dean 
-
-GRANT SELECT, UPDATE, INSERT, DELETE, REFERENCES ON course_catalogue to dean_role;
-GRANT SELECT, UPDATE, INSERT, DELETE ON prerequisites to dean_role;
-GRANT SELECT, UPDATE, INSERT, DELETE, REFERENCES ON department to dean_role;
-GRANT SELECT, UPDATE, INSERT, DELETE ON instructor to dean_role;
-GRANT SELECT, UPDATE, INSERT, DELETE ON time_slots to dean_role;
-GRANT SELECT, UPDATE, INSERT, DELETE ON student to dean_role;
-GRANT SELECT ON "student_transcript_student_id" to dean_role;
-GRANT SELECT ON "course_year_sem_sec_grades" to dean_role;
-GRANT SELECT, UPDATE, INSERT, DELETE ON dean_academics_ticket_table to dean_role;
-GRANT SELECT, UPDATE, INSERT, DELETE ON "student_ticket_table_student_id" to dean_role;
-
-
--- GRANT SELECT on course_offering_instid to dean_role;
--- to do student_transcript
+GRANT CONNECT ON DATABASE project1 TO faculty_role;
+GRANT SELECT ON course_catalogue to faculty_role;
+GRANT SELECT ON prerequisites to faculty_role;
+GRANT SELECT ON department to faculty_role;
+GRANT SELECT ON instructor to faculty_role;
+GRANT SELECT ON time_slots to faculty_role;
+GRANT SELECT ON student to faculty_role;
+GRANT SELECT, INSERT ON course_offering to faculty_role;
+GRANT SELECT ON "course_offering_year_semester" to faculty_role;
+GRANT SELECT ON "student_transcript_student_id" to faculty_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON "course_offering_grades_offering_id" to faculty_role;
+GRANT SELECT ON "student_ticket_table_student_id" to faculty_role;
+GRANT SELECT, INSERT, UPDATE ON "instructor_ticket_table_inst_id" to faculty_role;
+GRANT SELECT ON "program_cores_batch_number" to faculty_role;
+GRANT SELECT ON "open_elective_batch_number" to faculty_role;
+GRANT SELECT ON "program_elective_batch_number" to faculty_role;
+GRANT SELECT ON "science_core_batch_number" to faculty_role;
+GRANT SELECT ON "credit_requirenment_to_graduate" to faculty_role;
 
 
-
--- batch advisor
 GRANT CONNECT ON DATABASE project1 TO batch_advisor_role;
--- GRANT USAGE, CREATE ON SCHEMA project_schema TO batch_advisor_role;
-GRANT SELECT ON TABLE course_catalogue, prerequisites, student, instructor, taken, student_transcript, course_offered_grades TO batch_advisor_role;
--- to do student_transcript
--- SELECT ON TABLE course_offering_instid to batch_advisor_role;
-GRANT SELECT ON time_slots to batch_advisor_role;
+GRANT SELECT ON course_catalogue to batch_advisor_role;
+GRANT SELECT ON prerequisites to batch_advisor_role;
 GRANT SELECT ON department to batch_advisor_role;
-GRANT INSERT ON dean_academics_ticket_table to batch_advisor_role;
-GRANT SELECT, UPDATE, INSERT, DELETE ON batch_advisor_ticket_table to batch_advisor_role;
--- GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE ____ TO batch_advisor_role;
--- ALTER DEFAULT PRIVILEGES IN SCHEMA project_schema GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO batch_advisor_role;
+GRANT SELECT ON instructor to batch_advisor_role;
+GRANT SELECT ON time_slots to batch_advisor_role;
+GRANT SELECT ON student to batch_advisor_role;
+GRANT SELECT ON course_offering to batch_advisor_role;
+GRANT SELECT ON "course_offering_year_semester" to batch_advisor_role;
+GRANT SELECT ON "student_transcript_student_id" to batch_advisor_role;
+GRANT SELECT ON "course_offering_grades_offering_id" to batch_advisor_role;
+GRANT SELECT ON "instructor_ticket_table_inst_id" to batch_advisor_role;
+GRANT SELECT, INSERT, UPDATE ON batch_advisor_ticket_table to batch_advisor_role;
+GRANT SELECT ON "program_cores_batch_number" to batch_advisor_role;
+GRANT SELECT ON "open_elective_batch_number" to batch_advisor_role;
+GRANT SELECT ON "program_elective_batch_number" to batch_advisor_role;
+GRANT SELECT ON "science_core_batch_number" to batch_advisor_role;
+GRANT SELECT ON "credit_requirenment_to_graduate" to batch_advisor_role;
+
+
+GRANT CONNECT ON DATABASE project1 TO batch_advisor_role;
+GRANT SELECT ON course_catalogue to batch_advisor_role;
+GRANT SELECT ON prerequisites to batch_advisor_role;
+GRANT SELECT ON department to batch_advisor_role;
+GRANT SELECT ON instructor to batch_advisor_role;
+GRANT SELECT ON time_slots to batch_advisor_role;
+GRANT SELECT ON student to batch_advisor_role;
+GRANT SELECT ON course_offering to batch_advisor_role;
+GRANT SELECT ON "course_offering_year_semester" to batch_advisor_role;
+GRANT SELECT ON "student_transcript_student_id" to batch_advisor_role;
+GRANT SELECT ON "course_offering_grades_offering_id" to batch_advisor_role;
+GRANT SELECT ON "instructor_ticket_table_inst_id" to batch_advisor_role;
+GRANT SELECT, INSERT, UPDATE ON batch_advisor_ticket_table to batch_advisor_role;
+GRANT SELECT ON "program_cores_batch_number" to batch_advisor_role;
+GRANT SELECT ON "open_elective_batch_number" to batch_advisor_role;
+GRANT SELECT ON "program_elective_batch_number" to batch_advisor_role;
+GRANT SELECT ON "science_core_batch_number" to batch_advisor_role;
+GRANT SELECT ON "credit_requirenment_to_graduate" to batch_advisor_role;
+
+
+GRANT CONNECT ON DATABASE project1 TO dean_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON course_catalogue to dean_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON prerequisites to dean_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON department to dean_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON instructor to dean_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON time_slots to dean_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON student to dean_role;
+GRANT SELECT ON course_offering to dean_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON "course_offering_year_semester" to dean_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON "student_transcript_student_id" to dean_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON "course_offering_grades_offering_id" to dean_role;
+GRANT UPDATE ON "student_ticket_table_student_id" to dean_role;
+GRANT SELECT ON batch_advisor_ticket_table to dean_role;
+GRANT SELECT, INSERT, UPDATE ON dean_academics_ticket_table to dean_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON "program_cores_batch_number" to dean_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON "open_elective_batch_number" to dean_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON "program_elective_batch_number" to dean_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON "science_core_batch_number" to dean_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON "credit_requirenment_to_graduate" to dean_role;
